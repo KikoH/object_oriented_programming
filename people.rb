bdfe38907ed3bc670fef5510d6d14d01f4feec87
Kiko <mmhabib11@gmail.com>
2014-10-23 13:54:33 -0400

    Eighth and final step of exercise

--- people.rb
+++ people.rb
@@ -1,3 +1,4 @@
+#Parent Class
 class Person
 	attr_accessor :name
 	def greeting
@@ -6,18 +7,20 @@ class Person
 end
 
 
+#Class inheriting from Person
 class Student < Person
 	def learn
 		puts "I get it"
 	end
 end
-
+#Class inheriting from Person
 class Instructor < Person
 	def teach
 		puts "Everything in Ruby is an Object"
 	end
 end
 
+#Instantiating instructor and student and calling greet method from Person class
 instructor = Instructor.new
 instructor.name = "Chris"
 instructor.greeting
@@ -25,3 +28,10 @@ instructor.greeting
 student = Student.new
 student.name = "Christina"
 student.greeting
+
+#Calling the teach & learn methods
+instructor.teach
+student.learn
+
+#Calling teach on studen would not work because it is local to the instructor method
+student.teach

748c97ef6d40acc015d71b893d1fd191ff61bec0
Kiko <mmhabib11@gmail.com>
2014-10-23 13:49:57 -0400

    Sixth and seventh steps of calling greeting

--- people.rb
+++ people.rb
@@ -1,22 +1,27 @@
 class Person
-	def name(name)
-		@name = name
-	end
-
+	attr_accessor :name
 	def greeting
 		puts "Hi my name is #{@name}"
 	end
 end
 
 
-class Student
+class Student < Person
 	def learn
 		puts "I get it"
 	end
 end
 
-class Instructor
+class Instructor < Person
 	def teach
 		puts "Everything in Ruby is an Object"
 	end
 end
+
+instructor = Instructor.new
+instructor.name = "Chris"
+instructor.greeting
+
+student = Student.new
+student.name = "Christina"
+student.greeting
